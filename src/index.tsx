152b888f2270830cf2f8933f8810e39cd8eb3ddc
stewpika <hyy5752@163.com>
2022-11-17 13:25:58 +0200

    added routerdome 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SomeExampleComponent from './components/example/someExampleComponent';
+import MyFirstComponent from './components/myFirstComponent';
+import './index.module.scss';
 
 
 const root = ReactDOM.createRoot(
@@ -8,7 +16,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+      <Route path='example' element={<SomeExampleComponent />} />
+      <Route path='/first' element={<MyFirstComponent />} />
+    </Routes>
+  </BrowserRouter>
+</React.StrictMode>
 );
 
